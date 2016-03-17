3d777c6e6c5b9d6fe0ea1592c76e8e5a9eca75f3
franciscok <franciscok@artear.com>
2016-03-17 12:19:36 -0300

    excersise

---
+++ init.js
@@ -0,0 +1,10 @@
+//@TODO: que tenga un coso que lanze un calendario
+$( "#datepicker" ).algocalendario;
+$( "#datepicker" ).conestasopciones("option", "dateFormat", "dd/mm/yy");
+
+//@TODO: que pueda ser dropeable
+$(".task-container").algo;
+
+//@TODO: que pueda ser draggeable con revert valid y revertduration de 200
+$(".todo-task").algo;
+todo.init();
